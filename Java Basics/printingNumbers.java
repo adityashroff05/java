d592f62a9b6f344ebc24cc0dac88736ca8abbe23
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-04 13:03:26 +0530

    java code

---
+++ Java Basics/printingNumbers.java
@@ -0,0 +1,9 @@
+public class printingNumbers {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println(4+3);
+        System.out.println("4+3"); // inside double inverted coma everything behave like a text.
+    }
+    
+}
---
+++ Java Basics/takingnextline.java
@@ -0,0 +1,6 @@
+public class takingnextline {
+    public static void main(String[] args) {
+        System.out.println("we use ln for next line");
+    }
+}
+    
