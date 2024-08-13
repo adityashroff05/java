cb279037f94423e65b83623d8bd02a3d1b7802b1
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-13 22:45:02 +0530

    java codes

---
+++ Double data type/Question5.java
@@ -0,0 +1,9 @@
+public class Question5 {
+    public static void main(String[] args) {
+        double p = 100;
+        double r = 10;
+        double t = 2;
+        double si = (p*r*t)/100;
+        System.out.println(si); 
+    }
+}
