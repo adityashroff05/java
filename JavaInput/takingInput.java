f8ae75464845a2aaf7f23c2e83fd3b23e45611a8
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-13 22:57:16 +0530

    java code

--- JavaInput/takingInput.java
+++ JavaInput/takingInput.java
@@ -2,10 +2,11 @@ package JavaInput;
 import java.util.Scanner;
 public class takingInput {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int radius = sc.nextInt();
-        double pi = 3.1415;
-        double area = pi*radius*radius;
-        System.out.println("the area of circle is: "+area);
+        try (Scanner sc = new Scanner(System.in)) {
+            int radius = sc.nextInt();
+            double pi = 3.1415;
+            double area = pi*radius*radius;
+            System.out.println("the area of circle is: "+area);
+        }
     }
 }
