c282b32c4da914ccb9e383db7e896811af5651e0
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-15 00:03:16 +0530

    java codes

---
+++ Characterdatatype/characterdata.java
@@ -0,0 +1,19 @@
+//package Characterdatatype;
+import java.util.Scanner;
+public class characterdata {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = 'A';
+        System.out.println(ch);
+        int x = (int)ch; // This is called Typecasting.
+        System.out.println(x);
+    }
+//Some charactera are:-
+// qwertyuiopasdfghjklzxcvbnm;
+//QWERTYUIOPASDFGHJKLZXCVBNM
+//!@#$%^&*()_+/*-+ */
+//0123456789
+
+
+
+}
