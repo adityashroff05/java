810626f2540888c94ae9b76397726a4b324eed03
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-16 23:15:55 +0530

    java codes

---
+++ Conditional statement/if_else.java
@@ -0,0 +1,11 @@
+//package Conditional statement;
+import java.util.Scanner;
+public class if_else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a Number: ");
+        int n = sc.nextInt();
+        System.out.println(n);
+        sc.close();
+    }
+}
