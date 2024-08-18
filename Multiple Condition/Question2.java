219633225d9f7f420b254f4b1f7da1ae660a9bdd
Aditya Kumar <wartinsh1234@gmail.com>
2024-08-18 23:18:29 +0530

    java code

--- Multiple Condition/Question2.java
+++ Multiple Condition/Question2.java
@@ -1,3 +1,4 @@
+// Question: Take positive integer input and tell if it is divisible by 5 or 3?
 //package Multiple Condition;
 import java.util.Scanner;
 public class Question2 {
@@ -5,8 +6,8 @@ public class Question2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number : ");
         int x = sc.nextInt();
-        if(x%5==0 || x%3!=0){
-            System.out.println("Number is Divisible by 5 but not divisible with 3");
+        if(x%5==0 || x%3==0){
+            System.out.println("Number is Divisible by 5 & divisible with 3");
         }
         else{
             System.out.println("Not divisible by 3 & 5");
