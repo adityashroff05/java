a8927b9da0bd6d0a133b690ce4d05c14bbb7811d
Aditya Kumar <wartinsh1234@gmail.com>
2024-09-01 22:56:13 +0530

    java code

---
+++ Pattern_Printing/Pattern1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0;i<5;i++){
+            for(int j = 0;j<5;j++){
+                if(i==2 || j==2){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}
