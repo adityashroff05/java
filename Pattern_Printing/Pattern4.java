2c6f1643850e0822de1400e1a50e2e471e12bfa4
Aditya Kumar <wartinsh1234@gmail.com>
2024-09-02 23:19:26 +0530

    java code

--- Pattern_Printing/Pattern4.java
+++ Pattern_Printing/Pattern4.java
@@ -3,9 +3,11 @@ public class Pattern4 {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     for(int i=0;i<4;i++){
+        int count =1;
         for(int j=0;j<4;j++){
             if(i+j>=3){
-                System.out.print("* ");
+                System.out.print(count+" ");
+                count++;
             }else{
                 System.out.print(" ");
             }
